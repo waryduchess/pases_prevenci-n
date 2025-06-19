bbb43a63e8a1f331f9cf1a012436210a8efb4fba
Erik Garcia <>
2025-06-19 10:37:06 -0600

    correcion de choques en el codigo

--- PASE/PASE/Vistas/Form1.Designer.cs
+++ PASE/PASE/Vistas/Form1.Designer.cs
@@ -506,17 +506,17 @@
             // cbxNmbreHotel
             // 
 
-            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.cbxNmbreHotel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxNmbreHotel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxNmbreHotel.FormattingEnabled = true;
+            this.cbxNmbreHotel.Items.AddRange(new object[] {
             "XPM",
             "FPM",
             "BPM"});
-            this.comboBox1.Location = new System.Drawing.Point(90, 60);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(167, 28);
-            this.comboBox1.TabIndex = 35;
+            this.cbxNmbreHotel.Location = new System.Drawing.Point(90, 60);
+            this.cbxNmbreHotel.Name = "comboBox1";
+            this.cbxNmbreHotel.Size = new System.Drawing.Size(167, 28);
+            this.cbxNmbreHotel.TabIndex = 35;
 
             // 
             // frmArticulos
